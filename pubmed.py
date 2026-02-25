c80275098c968b4a7c0e1c9508988eb315c90ad4
Harold229 <73756893+Harold229@users.noreply.github.com>
2026-02-25 09:35:15 +0100

    fix: add error handling and cache for PubMed API

--- pubmed.py
+++ pubmed.py
@@ -1,31 +1,38 @@
 import requests 
 import xml.etree.ElementTree as ET
+import streamlit as st
+
+
+@st.cache_data(ttl = 3600)
 def get_mesh_terms(term: str) -> list:
     """
     Prend un terme de recherche et renvoie une liste de termes Mesh associés depuis l'API NCBI
 
     """
-
-    base_url =  "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-    params = {
-        "db": "pubmed",
-        "term": term,
-        "retmode": "xml",
+    try:
+        base_url =  "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+        params = {
+            "db": "pubmed",
+            "term": term,
+            "retmode": "xml",
         "retmax":0
     }
 
-    response = requests.get(base_url, params = params)
-    root = ET.fromstring(response.text)
-    query_translation = root.find("QueryTranslation")
+        response = requests.get(base_url, params = params, timeout = 10)
 
-    if query_translation is not None:
-        return query_translation.text
-    return f'"{term}"[ALL fields]'
+        if response.status_code != 200 or not response.text.strip():
+            return f'"{term}"[ALL fields]'
 
-# Test rapide
-if __name__ == "__main__":
-    result = get_mesh_terms("diabetes")
-    print(result)
+        root = ET.fromstring(response.text)
+        query_translation = root.find("QueryTranslation")
+
+        if query_translation is not None:
+            return query_translation.text
+        return f'"{term}"[ALL fields]'
+    except Exception:
+           return f'"{term}"[All Fields]'
+
+
 
 
 
